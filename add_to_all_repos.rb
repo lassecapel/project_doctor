337d1f5c1bd02422a6c0601ba808b837977b615f
Egbert Veenstra <egbertveenstra@gmail.com>
2013-02-12 21:31:07 +0100

    remove add to all repos script

--- add_to_all_repos.rb
+++
@@ -1,6 +0,0 @@
-`heroku apps`.each_line do |line|
-  next if line.strip.empty?
-  next if line =~ /\A===/
-  app_name = line[/^[^\s]+/]
-  `heroku sharing:add egbert@brightin.nl --app #{app_name}`
-end
